c6a86512855b615c4f81dcf1514ef9607f72d63a
kubista9 <jakubkuka07@gmail.com>
2023-10-30 11:20:30 +0100

    business logic

--- businessLogic/src/main/java/org/example/repository/PatientRepository.java
+++
@@ -1,8 +0,0 @@
-package org.example.repository;
-
-import group5.hospital.entity.Patient;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PatientRepository extends CrudRepository<Patient, Integer> {
-
-}
